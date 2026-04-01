18fe6e1b6da28252028b52a262e43130970ef00d
Francisco Blazquez <fabw79@gmail.com>
2026-04-01 04:21:36 +0200

    feat: agrega actividad 1 clase Libro

--- actividad_m4_l2/poo_avanzado.py
+++ actividad_m4_l2/poo_avanzado.py
@@ -1,1 +1,14 @@
+class Libro:
+    def __init__(self, titulo, autor, anio_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
 
+    def mostrar_info(self):
+        print("Título:", self.titulo)
+        print("Autor:", self.autor)
+        print("Año de publicación:", self.anio_publicacion)
+
+
+libro1 = Libro("Cien años de soledad", "Gabriel García Márquez", 1967)
+libro1.mostrar_info()

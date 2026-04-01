08073e1c910413d1cdea8234ff1fbe703b555fe8
Francisco Blazquez <fabw79@gmail.com>
2026-04-01 06:02:08 +0200

    feat: agrega clase Persona

--- poo_sistema_educacional/main.py
+++ poo_sistema_educacional/main.py
@@ -1,1 +1,10 @@
-# Sistema educacional en Python
+class Persona:
+    def __init__(self, nombre, apellido, rut):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.rut = rut
+
+    def mostrar_datos(self):
+        print("Nombre:", self.nombre)
+        print("Apellido:", self.apellido)
+        print("RUT:", self.rut)

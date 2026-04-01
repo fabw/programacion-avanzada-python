17a33bcc7cdff9cc477fc035ca027d28089d5bc4
Francisco Blazquez <fabw79@gmail.com>
2026-04-01 04:23:56 +0200

    eat: agrega actividad 2 accesadores y mutadores

--- actividad_m4_l2/poo_avanzado.py
+++ actividad_m4_l2/poo_avanzado.py
@@ -1,3 +1,4 @@
+# Actividad 1: Definición de una clase con constructor
 class Libro:
     def __init__(self, titulo, autor, anio_publicacion):
         self.titulo = titulo
@@ -12,3 +13,40 @@ class Libro:
 
 libro1 = Libro("Cien años de soledad", "Gabriel García Márquez", 1967)
 libro1.mostrar_info()
+
+# Actividad 2: Métodos accesadores y mutadores
+class LibroEncapsulado:
+    def __init__(self, titulo, autor, anio_publicacion):
+        self._titulo = titulo
+        self._autor = autor
+        self._anio_publicacion = anio_publicacion
+
+    def mostrar_info(self):
+        print("Título:", self._titulo)
+        print("Autor:", self._autor)
+        print("Año de publicación:", self._anio_publicacion)
+
+    def get_titulo(self):
+        return self._titulo
+
+    def set_titulo(self, nuevo_titulo):
+        self._titulo = nuevo_titulo
+
+    def get_anio_publicacion(self):
+        return self._anio_publicacion
+
+    def set_anio_publicacion(self, nuevo_anio):
+        self._anio_publicacion = nuevo_anio
+
+
+print("\nACTIVIDAD 2")
+libro2 = LibroEncapsulado("El principito", "Antoine de Saint-Exupéry", 1943)
+libro2.mostrar_info()
+
+print("Título actual:", libro2.get_titulo())
+libro2.set_titulo("El principito - Edición especial")
+print("Título modificado:", libro2.get_titulo())
+
+print("Año actual:", libro2.get_anio_publicacion())
+libro2.set_anio_publicacion(1944)
+print("Año modificado:", libro2.get_anio_publicacion())

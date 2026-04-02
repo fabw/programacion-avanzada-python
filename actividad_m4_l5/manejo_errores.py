d274683a7a3cec5c5673f28137b8a6a1ff40afa7
Francisco Blazquez <fabw79@gmail.com>
2026-04-02 05:18:47 +0200

    feat: agrega estructura base de manejo_errores

--- actividad_m4_l5/manejo_errores.py
+++ actividad_m4_l5/manejo_errores.py
@@ -1,1 +1,14 @@
+class EdadInvalidaError(Exception):
+    pass
 
+def dividir():
+    pass
+
+def validar_edad(edad):
+    pass
+
+def simulacion_archivo():
+    pass
+
+if __name__ == "__main__":
+    dividir()

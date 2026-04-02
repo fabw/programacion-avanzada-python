83b1dfbd64ea0e4109616decc1a44658accce61a
Francisco Blazquez <fabw79@gmail.com>
2026-04-02 06:06:00 +0200

    feat: implementa división sin manejo de errores

--- actividad_m4_l5/manejo_errores.py
+++ actividad_m4_l5/manejo_errores.py
@@ -2,7 +2,10 @@ class EdadInvalidaError(Exception):
     pass
 
 def dividir():
-    pass
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = num1 / num2
+    print("El resultado es:", resultado)
 
 def validar_edad(edad):
     pass
